7dbfdea790fd31773029345f6f0199326e18bfda
Sadiq <sadiq.said@student.moringaschool.com>
2023-12-07 17:30:52 +0300

    Lab completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,17 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+
+        response = requests.get(url)
+        return response.content
+
 
     def load_json(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()  # Raises an HTTPError if the response was an HTTP error
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error during requests to {self.url} : {str(e)}")
+            return None
